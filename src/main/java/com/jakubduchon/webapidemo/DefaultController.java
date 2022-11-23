c6c96d8d0cfee942d93a9198132ed131a0bf144d
banny99 <benjamin.bartosik@gmail.com>
2022-11-23 09:41:25 +0100

    first change check

--- src/main/java/com/jakubduchon/webapidemo/DefaultController.java
+++ src/main/java/com/jakubduchon/webapidemo/DefaultController.java
@@ -8,6 +8,6 @@ public class DefaultController {
 
     @GetMapping("/")
     String health() {
-        return "Hello students! :) The application was deployed using GitHub Actions! <3";
+        return "BB - Hello students! :) The application was deployed using GitHub Actions! <3";
     }
 }
